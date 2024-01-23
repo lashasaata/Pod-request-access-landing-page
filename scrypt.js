a4d0cfa80d5e26738b9a57efdda0f08004859cc2
lashasaata <lashasaata@gmail.com>
2024-01-23 08:42:28 -0800

    add functional in js

---
+++ scrypt.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const p = document.getElementById("error");
+
+let inputValue = "";
+input.addEventListener("input", (event) => {
+  inputValue = event.target.value;
+  if (inputValue.length > 0) {
+    input.style.opacity = "1";
+  } else {
+    input.style.opacity = "0.5";
+  }
+});
+function requestAccess() {
+  if (inputValue.length > 0) {
+    p.style.display = "block";
+  }
+}
